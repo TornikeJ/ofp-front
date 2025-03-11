de77d41f99814d9e138f7283b92790fc605dd85c
TornikeJ <t.janashia11@gmail.com>
2025-03-12 02:23:39 +0400

    feat. add header, nav, styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NgOptimizedImage, UpperCasePipe} from '@angular/common';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [UpperCasePipe, MatButton, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ofp-front';
+
+  ngOnInit(): void {
+  }
 }
